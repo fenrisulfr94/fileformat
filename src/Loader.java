36033e61cb626b8ad905a1321d613c2afdbb7cad
Gareth Gill <garethmgill@gmail.com>
2014-02-19 20:51:38 +0000

    Changed case statements to allow for usage of the Bumper enum in the Loader class.
    Also added an ObjectOutputStream to write StandardFile object directly
    to a file, namely "save".

--- src/Loader.java
+++ src/Loader.java
@@ -1,8 +1,7 @@
-
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 import java.util.Scanner;
-
 
 public class Loader
 {
@@ -11,7 +10,24 @@ public class Loader
 	public static int squareCount, circleCount, lfCount, rfCount, triCount;
 	public static FileOutputStream fout;
 	public static ObjectOutputStream oos;
-	
+
+	public static void writeGiz()
+	{
+		try
+		{
+			File file = new File("save.txt");
+			oos = new ObjectOutputStream(new FileOutputStream(file));
+			oos.writeObject(sf);
+			oos.flush();
+			oos.close();
+			System.out.println("Saved!");
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
 	public static void main(String[] args) throws Exception
 	{
 		squareCount = 1;
@@ -19,60 +35,75 @@ public class Loader
 		lfCount = 1;
 		rfCount = 1;
 		triCount = 1;
-		
-		/** Depending on the type of gizmo,
-		 * 	it will be saved as:
-		 * 		[shape] [letter & no.] 
+
+		/**
+		 * Depending on the type of gizmo, it will be saved as: [shape] [letter
+		 * & no.]
 		 */
-		
+
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Please enter a gizmo (i.e. Square, Triangle, Circle): ");
-		String input = sc.nextLine();
+		System.out
+				.println("Please enter a gizmo (i.e. Square, Triangle, Circle): ");
+		String s = sc.next();
+		giz = Bumper.valueOf(s);
+		
+		System.out.println("Please enter an x co-ordinate: ");
+		int x = sc.nextInt();
+		
+		System.out.println("Please enter a y co-ordinate: ");
+		int y = sc.nextInt();
 		sc.close();
-		
-		switch(input)
+
+		switch (giz)
 		{
-		
-		case "Square":
+
+		case SQUARE:
 			sf.setGizmo("Square");
 			sf.setTag("S" + squareCount);
+			sf.setCoX(x);
+			sf.setCoY(y);
 			squareCount++;
-			
-		case "Circle":
+			writeGiz();
+			break;
+
+		case CIRCLE:
 			sf.setGizmo("Circle");
 			sf.setTag("C" + circleCount);
+			sf.setCoX(x);
+			sf.setCoY(y);
 			circleCount++;
-			
-		case "Left Flipper":
+			break;
+
+		case LEFT_FLIPPER:
 			sf.setGizmo("LeftFlipper");
 			sf.setTag("LF" + lfCount);
+			sf.setCoX(x);
+			sf.setCoY(y);
 			lfCount++;
-			
-		case "Right Flipper":
+			break;
+
+		case RIGHT_FLIPPER:
 			sf.setGizmo("RightFlipper");
 			sf.setTag("RL" + rfCount);
-			
-		case "Triangle":
+			sf.setCoX(x);
+			sf.setCoY(y);
+			rfCount++;
+			break;
+
+		case TRIANGLE:
 			sf.setGizmo("Triangle");
 			sf.setTag("T" + triCount);
-			
+			sf.setCoX(x);
+			sf.setCoY(y);
+			triCount++;
+			break;
+
 		default:
-			try
-			{
-				fout = new FileOutputStream("~/Saves/save.ser");
-				oos = new ObjectOutputStream(fout);
-				oos.writeObject(sf);
-				oos.close();
-				System.out.println("Saved!");
-			}
-			catch (Exception e)
-			{
-				e.printStackTrace();
-			};
-			
+			writeGiz();
+			break;
+
 		}
-			
+		
 
 	}
-
 }
